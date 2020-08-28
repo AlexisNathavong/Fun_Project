b6521be3011f09c6bf3a65f9b1f7d7c99d5f9805
Alexis <alexispanyathong@gmail.com>
2020-08-28 16:53:15 -0500

    Added:
    I added the Login reducers and created the login form. Now I am making a plan on creating the backend to connect to the front end.

--- fun_project/src/components/Login.js
+++ fun_project/src/components/Login.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+import { reducer, initialState } from '../reducers/LoginReducer';
+import axios from 'axios';
+
 
 const Login = () => {
+    const [user, setNewUser] = useState({username: '', password: ''});
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleChanges = e => {
+        setNewUser({...user, [e.target.name]: e.target.value});
+    };
+
+    const login = e => {
+        e.preventDefault();
+
+        axios.post('', user)
+            .then(res => {
+                console.log('login success: ', res)
+
+                localStorage.setItem('token', res.data.payload);
+
+                dispatch({ type: 'LOGIN', payload: res.data});
+            })
+            .catch(err => {
+                console.log('login fail: ', err.res)
+            })
+        setNewUser('');
+    }
+    console.log(state);
 
     return (
-        <div>
-            <h1>Login...</h1>
-        </div>
+        <form>
+            <div className='login-group'>
+                <label>Username: </label>
+
+                <input
+                    className='login-group'
+                    type='username'
+                    name='username'
+                    value={user.username}
+                    onChange={handleChanges}
+                />
+                
+            </div>
+
+            <div className='login-group'>
+                <label>Password: </label>
+
+                <input
+                    className='login-group'
+                    type='password'
+                    name='password'
+                    value={user.password}
+                    onChange={handleChanges}
+                />
+
+            </div>
+
+            <button onClick={login}>Login</button>
+
+        </form>
     )
 };
 
